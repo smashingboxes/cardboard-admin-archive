6eafb711f239dff0c2c4d0ff3d9cd09e9362cebc
Michael Elfassy <michael@smashingboxes.com>
2013-05-07 14:46:01 -0400

    better string logic

--- app/models/cardboard/field/string.rb
+++ app/models/cardboard/field/string.rb
@@ -1,11 +1,17 @@
 module Cardboard
   class Field::String < Field
-    validates :value, :length => { :maximum => 255 }
+    # validates :value, :length => { :maximum => 255 }
     validates :value, presence:true, :if => :required_field?
-
+    before_validation :truncate_long_string
 
     def default
       "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur"
+    end
+
+    private
+
+    def truncate_long_string
+      self.value = self.value[1..255] if value
     end
   end
 end
