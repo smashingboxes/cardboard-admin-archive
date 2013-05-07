1df2d371c3740ea2c0bb45fe9bdf2a0f56aaf679
Michael Elfassy <michael@smashingboxes.com>
2013-05-07 10:23:49 -0400

    ability to edit controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,27 +5,33 @@ class PagesController < ApplicationController
     else
       @page = Cardboard::Page.root
     end
+    # call controller hook
+    self.send(@page.identifier) if self.respond_to? @page.identifier
+
     render_main_app_page @page
   end
 
 private
 
-  def edit_link
-    cardboard.edit_page_path(@page)
-  end
-  helper_method :edit_link
+  # def edit_link
+  #   cardboard.edit_page_path(@page)
+  # end
+  # helper_method :edit_link
 
   def current_page
     @page
   end
   helper_method :current_page
 
+
   def render_main_app_page(page)
     #TODO: Make the layout name variable
     render "pages/#{page.identifier}"#, layout: "layouts/application"
+
   rescue ActionView::MissingTemplate => e
     @missing_file = e.path
     render "cardboard/pages/error"#, layout: "layouts/application"
+
   rescue NoMethodError => e
     raise ActionController::RoutingError.new("Page Not Found")
   end
