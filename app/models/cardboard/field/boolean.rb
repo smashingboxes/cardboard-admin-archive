0df2caa89f8d7af4703cc0ae149fd8f0374786c5
Michael Elfassy <michael@smashingboxes.com>
2013-05-10 10:31:00 -0400

    proper error message for boolean validation error

--- app/models/cardboard/field/boolean.rb
+++ app/models/cardboard/field/boolean.rb
@@ -1,18 +1,21 @@
 module Cardboard
   class Field::Boolean < Field
-    validates :value_uid, :inclusion => { :in => %w(yes true no false 0 1 y n t f) + [true, false], :message => "%{value} is not a boolean" }, :allow_nil => true
+    # validates :value, :inclusion => { :in => %w(yes true no false 0 1 y n t f) + [true, false], :message => "is not a boolean" }, :allow_nil => true
     # don't validate presence, nil == false
+    validate :is_boolean?
 
     def value
       super.to_s.to_boolean
     end
 
-    # def value=(val)
-    #   self[:value_uid] = val
-    # end
-
     def default
       [true, false].sample
+    end
+
+    private
+
+    def is_boolean?
+      self.value rescue errors.add(:value, "is not a valid boolean")
     end
   end
 end
