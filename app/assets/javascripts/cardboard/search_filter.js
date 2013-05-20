0db042ecff0e45ec05607ef7356098c7e5eba1fa
Michael Elfassy <michael@smashingboxes.com>
2013-05-20 14:04:38 -0400

    pjax now on search too!

--- app/assets/javascripts/cardboard/search_filter.js
+++ app/assets/javascripts/cardboard/search_filter.js
@@ -1,26 +1,26 @@
 $(function(){
+   var ransack_options;
   
-  var $advanced_field_select = $("select#advanced_field");
-  var $ransack_select = $("select#ransack_options")
-  var $ransack_options = $("select#ransack_options").html();
+  $(document).on("pjax:click, ready", function(e){
+    ransack_options = $("select#ransack_options").html();
+    $("select#advanced_field").trigger("change");
+  })
 
-  $advanced_field_select.change(function(){
-    var type = $advanced_field_select.find(":selected").data("type");
-    var options = $($ransack_options).filter("optgroup[label='" + type + "']").html()
-    $ransack_select.html(options);
-    $ransack_select.trigger("change");
+  $(document).on('change', "select#advanced_field", function(){
+    var type = $(this).find(":selected").data("type");
+    var options = $(ransack_options).filter("optgroup[label='" + type + "']").html()
+    $("select#ransack_options").html(options).trigger("change");
   });
-  $advanced_field_select.trigger("change");
-
-  $ransack_select.change(function(){
-    var field_name = $advanced_field_select.find(":selected").val() +"_" + $(this).val();
+  
+  $(document).on('change',"select#ransack_options", function(){
+    var field_name = $("select#advanced_field").find(":selected").val() +"_" + $(this).val();
     $("#advanced_query").attr("name", "q["+field_name+"]").val("");
   });
-  $("#advanced_search_link").click(function(){
+  $(document).on('click', "#advanced_search_link", function(){
     $("#simple_search").hide();
     $("#advanced_search").show();
   });
-  $("#simple_search_link").click(function(){
+  $(document).on('click', "#simple_search_link", function(){
     $("#advanced_search").hide();
     $("#simple_search").show();
   });
