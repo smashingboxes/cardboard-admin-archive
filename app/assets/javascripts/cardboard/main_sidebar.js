57e29e24dcb5db3ce90d6d9add01147492829dcb
Pierre Lebrun <anthonylebrun@gmail.com>
2013-06-17 14:40:16 -0400

    updating style and added sliding animation

--- app/assets/javascripts/cardboard/main_sidebar.js
+++ app/assets/javascripts/cardboard/main_sidebar.js
@@ -1,31 +1,31 @@
 $(function(){
   $("a#nav_dashboard_link").click(function(e){
     e.preventDefault();
-    $("#content_sidebar").toggle();
+    $("#content_sidebar").toggleClass('toggle');
     $('#content').toggleClass('toggle');
     // $(this).toggleClass('active');
   });
 
 
   // Filter page sidebar
-  $('#sidebar_page_search').search('.indent', function(on) {
+  $('#sidebar_page_search').search('.page', function(on) {
     on.all(function(results) {
       var size = results ? results.size() : 0
     });
 
     on.reset(function() {
       $('#sidebar_page_search_none').hide();
-      $('.indent').show();
+      $('.page').show();
     });
 
     on.empty(function() {
       $('#sidebar_page_search_none').show();
-      $('.indent').hide();
+      $('.page').hide();
     });
 
     on.results(function(results) {
       $('#sidebar_page_search_none').hide();
-      $('.indent').hide();
+      $('.page').hide();
       results.show();
     });
   });
