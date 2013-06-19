149cd876d2f35f53bf0f1620cb2b739c84c0ab33
Pierre Lebrun <anthonylebrun@gmail.com>
2013-06-19 08:58:43 -0400

    sortable pages enabled

--- app/assets/javascripts/cardboard/content_sidebar.js
+++ app/assets/javascripts/cardboard/content_sidebar.js
@@ -2,11 +2,25 @@ $(document).ready(function(){
   
   var updateURL = $('#content_pages').attr('data-update-url');
   
-  $('#content_pages, .page_group').sortable({
+  var sortableSettings = {
     axis: "y",
     handle: ".sort_handle",
-    update: function () {
-      $.post(updateURL, $(this).sortable('serialize', {key: "pages[]"}));
-    }
-  });
+    update: postRefresh
+  }
+  
+  $('#content_pages, .page_group').sortable(sortableSettings);
+  
+  function postRefresh(event, ui) {
+    // animate save
+    var content = $(ui.item).html();
+    $(ui.item).html('<div class="link_wrap"><a href="#" class="offset-0 saving">Saved!</a></div>');
+    setTimeout(function(){
+      $(ui.item).html(content);
+      $('#content_pages, .page_group').sortable(sortableSettings);
+    }, 800);
+    
+    // post results to server
+    $.post(updateURL, $(this).sortable('serialize', {key: "pages[]"}));
+  }
+  
 });
