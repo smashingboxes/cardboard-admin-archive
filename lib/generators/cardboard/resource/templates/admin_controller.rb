a16d65c60316fca3effa705f5bfb4786b1d2d7e8
Michael Elfassy <michaelelfassy@gmail.com>
2013-06-28 15:10:28 -0400

    resources default redirect to index
    don't provide show action since it's most likely not used (can still be
    added by user)

--- lib/generators/cardboard/resource/templates/admin_controller.rb
+++ lib/generators/cardboard/resource/templates/admin_controller.rb
@@ -1,8 +1,9 @@
 class <%= controller_name.camelize %> < Cardboard::ResourceController
   # uses inherited_resources. For more info see https://github.com/josevalim/inherited_resources
+  actions :all, :except => [:show]
 
-  # If you are using strong parameters uncomment the method below
-  # def permitted_strong_parameters
-  #   :all #or an array of parameters, example: [:name, :email]
-  # end
+  # If you are not using strong parameters comment the method below
+  def permitted_strong_parameters
+    :all #or an array of parameters, example: [:name, :email]
+  end
 end
