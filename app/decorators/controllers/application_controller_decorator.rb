acb0c8e63e59557cac4e85e601de81f5be3754e7
Michael Elfassy <michaelelfassy@gmail.com>
2013-07-25 12:21:38 -0400

    [bug] fix asset precompilation in production

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -3,7 +3,7 @@ ApplicationController.class_eval do
   # helper Cardboard::PublicHelper
 
   def current_page
-    nil #this will be overwritten only for carboard pages
+    nil #this will be overwritten only for cardboard pages
   end
   helper_method :current_page
 end
--- lib/cardboard/engine.rb
+++ lib/cardboard/engine.rb
@@ -67,7 +67,7 @@ module Cardboard
 
     if Rails.version > "3.1"
       initializer "precompile hook", :group => :all do |app|
-        app.config.assets.precompile += ["cardboard/cardboard.js", "cardboard/cardboard.css"]
+        app.config.assets.precompile += ["cardboard.js", "cardboard.css"]
       end
     end
 
