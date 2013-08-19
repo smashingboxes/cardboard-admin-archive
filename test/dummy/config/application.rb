12d2d04bd3fdd3d9e8058c9c32443bff4fb7acc8
Michael Elfassy <michaelelfassy@gmail.com>
2013-08-19 14:54:15 -0400

    fix for tests

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(:default, Rails.env)
+require 'cardboard_cms'
 
 module Dummy
   class Application < Rails::Application
