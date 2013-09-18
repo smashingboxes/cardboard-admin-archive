86d4f4cfd9c2beaf9f7589cfe85758eac2b4cd6d
Michael Elfassy <michael@smashingboxes.com>
2013-09-18 13:02:22 -0400

    moved page_path to application controller

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -6,4 +6,12 @@ ApplicationController.class_eval do
     nil #this will be overwritten only for cardboard pages
   end
   helper_method :current_page
+
+  # Example: 
+  #  page_path(:home)
+  #   
+  def page_path(identifier)
+    Cardboard::Page.where(identifier: identifier.to_s).first.try(:url)
+  end
+  helper_method :page_path
 end
