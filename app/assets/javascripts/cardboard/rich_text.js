b6938c6824e8d609c70c7ecb5202bb749f26e2ac
Michael Elfassy <michael@smashingboxes.com>
2013-12-17 13:34:54 -0500

    fix bug with multiple RTE on one page

--- app/assets/javascripts/cardboard/rich_text.js
+++ app/assets/javascripts/cardboard/rich_text.js
@@ -56,10 +56,19 @@ var rich_text_editor_defaults = {
 }
 
 $(document).on("ready pjax:end", function(e){
-  // console.log($(".wysihtml5").length);
   if($(".wysihtml5").length) {
     $('.wysihtml5').wysihtml5(rich_text_editor_defaults);
   }
+
+  // TODO: find a better way to handle multiple RTE on a single page
+  $(".bootstrap-wysihtml5-insert-link-modal .tab-content div").each(function (index) {
+    $(this).attr("id", $(this).attr("id") + index.toString());            
+  });
+  $(".bootstrap-wysihtml5-insert-link-modal .nav li a").each(function (index) {
+    $(this).attr("href", $(this).attr("href") + index.toString());
+    if(index % 3 == 0) $(this).tab('show');
+  });
+  // END TODO
 });
 
 $(document).on('cocoon:after-insert', function(e, insertedItem) {
@@ -68,12 +77,14 @@ $(document).on('cocoon:after-insert', function(e, insertedItem) {
   }
 });
 
-$(document).on('click', '.bootstrap-wysihtml5-insert-link-modal #link_pages a', function(e){
-  e.preventDefault;
+
+
+$(document).on('click', '.bootstrap-wysihtml5-insert-link-modal .link_pages a', function(e){
+  e.preventDefault();
+  console.log("clicked");
   modal = $(this).closest(".bootstrap-wysihtml5-insert-link-modal");
   modal.find("input.bootstrap-wysihtml5-insert-link-url").val($(this).data("url"));
   modal.find("#insert_link").click();
-  return false;
 });
 
 $(document).on('change', '.bootstrap-wysihtml5-insert-link-modal .tab-pane input', function(){
