2aff88384a9073a8cc2b055693045e4ba8aeec19
Jack Forrest <jack@jrforrest.net>
2014-05-07 12:10:02 -0400

    Allow singular resources in main menu
    issue #73

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,14 @@ Cardboard::Engine.routes.draw do
   
   scope as: 'cardboard' do
     #generate routes for custom cardboard resources controllers
-    Cardboard.resource_controllers.map{|x|x.controller_name}.each do |controller|
-      if controller.singularize == controller && controller.pluralize != controller
-        resource controller #singular controller
+    Cardboard.resource_controllers.each do |controller|
+      if controller.singular?
+        # Singular controller names are non-standard in rails.  We must specify the
+        # controller explicitly here to keep the name from 
+        # being pluralized in the route.
+        resource controller.controller_name, controller: controller.controller_name
       else
-        resources controller
+        resources controller.controller_name
       end
     end
   end
