02521571772cbc8688a3d2de5d1a213457705b5c
Michael Elfassy <michael@smashingboxes.com>
2014-05-12 09:53:11 -0400

    don't sanitize rich text for admins

--- app/models/cardboard/field/rich_text.rb
+++ app/models/cardboard/field/rich_text.rb
@@ -1,7 +1,7 @@
 module Cardboard
   class Field::RichText < Field
     validate :is_required
-    before_save :sanitize_value
+    # before_save :sanitize_value
 
     def value
       self.value_uid.try(:html_safe)
@@ -13,10 +13,10 @@ module Cardboard
 
     private
     
-    def sanitize_value
-      return nil unless value_changed?
-      self.value = ActionController::Base.helpers.sanitize(self.value, :tags => %w(strong b i u em br p div span ul ol li a pre code blockquote h1 h2 h3), :attributes => %w(class style href src width height alt))
-    end
+    # def sanitize_value
+    #   return nil unless value_changed?
+    #   self.value = ActionController::Base.helpers.sanitize(self.value, :tags => %w(strong b i u em br p div span ul ol li a pre code blockquote h1 h2 h3 table), :attributes => %w(class style href src width height alt))
+    # end
 
     def is_required
       errors.add(:value, "is required") if required_field? && ActionController::Base.helpers.strip_tags(value_uid).blank?
