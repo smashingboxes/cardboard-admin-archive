1bece5e7bb1475c10a4a212e8965c374b7a69953
BrandonMathis <BeMathis@gmail.com>
2014-10-24 15:20:44 -0400

    Remove commented out code

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -11,32 +11,6 @@ SimpleForm.setup do |config|
     b.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
   end
 
-  # config.wrappers :prepend, :tag => 'div', :class => "input-group", :error_class => 'error' do |b|
-  #   b.use :html5
-  #   b.use :placeholder
-  #   b.use :label
-  #   b.wrapper :tag => 'div', :class => 'form-controls' do |input|
-  #     input.wrapper :tag => 'div', :class => 'input-prepend' do |prepend|
-  #       prepend.use :input
-  #     end
-  #     input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-  #     input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-  #   end
-  # end
-
-  # config.wrappers :append, :tag => 'div', :class => "input-group", :error_class => 'error' do |b|
-  #   b.use :html5
-  #   b.use :placeholder
-  #   b.use :label
-  #   b.wrapper :tag => 'div', :class => 'form-controls' do |input|
-  #     input.wrapper :tag => 'div', :class => 'input-append' do |append|
-  #       append.use :input
-  #     end
-  #     input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-  #     input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-  #   end
-  # end
-
   # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
   # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
   # to learn about the different styles for forms and inputs,
