325af0d328c5c4e161bdf70615b265d7fd65d437
Ian Donovan <idonovan90@gmail.com>
2014-12-22 17:18:09 -0500

    Link_to override sets links to be opened in new tabs when they are non-cardboard

--- app/helpers/cardboard/application_helper.rb
+++ app/helpers/cardboard/application_helper.rb
@@ -1,4 +1,17 @@
 module Cardboard
   module ApplicationHelper
   end
+
+  module ActionView::Helpers::UrlHelper
+    def link_to_with_new_tab(name = nil, options = nil, html_options = nil, &block)
+      if options.is_a?(String)
+        if request.fullpath =~ /cardboard\// && options !~ /cardboard\/|#{ Cardboard.user_class.name.underscore }\//
+          html_options ||= {}
+          html_options[:target] = "_blank"
+        end
+      end
+      link_to_without_new_tab(name, options, html_options, &block)
+    end
+    alias_method_chain :link_to, :new_tab
+  end
 end
