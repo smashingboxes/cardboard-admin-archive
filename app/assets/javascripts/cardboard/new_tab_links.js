efd42fe3ed117eaa23749986dc4de575bc81de42
Ian Donovan <idonovan90@gmail.com>
2015-01-05 16:24:46 -0500

    change variable names, use strictertype checking

--- app/assets/javascripts/cardboard/new_tab_links.js
+++ app/assets/javascripts/cardboard/new_tab_links.js
@@ -1,8 +1,8 @@
 $(document).on("page:update", function() {
   $("a").click(function() {
-    var link_host = this.href.split('/')[3];
-    var document_host = document.location.href.split('/')[3];
-    if (link_host != document_host) {
+    var link_root = this.href.split('/')[3];
+    var document_root = document.location.href.split('/')[3];
+    if (link_root !== document_root) {
       window.open(this.href, '_blank');
       return(false);
     }
