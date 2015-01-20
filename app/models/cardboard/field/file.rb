4477a17dc3326d24865dae0aae3ff1a2a1d9fbd9
Leonel Galan <leonelgalan@gmail.com>
2015-01-20 17:01:45 -0500

    Whitespace and some bling :sparkles:

--- app/models/cardboard/field/file.rb
+++ app/models/cardboard/field/file.rb
@@ -1,17 +1,16 @@
 module Cardboard
   class Field::File < Field
-
     dragonfly_accessor :value
 
-    validates_size_of :value, :maximum => 20.megabytes #20000.kilobytes #TODO: move size to gem settings
-    validate :is_required #for some reason this is different from image
+    validates_size_of :value, maximum: 20.megabytes #TODO: move size to gem settings
+    validate :is_required
 
     def value
       return nil unless value_uid
-        Dragonfly.app.fetch_file(value_uid)
       if value_uid =~ value_uid_regex
+        Dragonfly.app.fetch_file value_uid
       else
-        Dragonfly.app.fetch(value_uid)
+        Dragonfly.app.fetch value_uid
       end
     end
 
--- app/models/cardboard/field/image.rb
+++ app/models/cardboard/field/image.rb
@@ -1,24 +1,21 @@
 module Cardboard
-
-    dragonfly_accessor :value do 
-      after_assign  :resize_image 
   class Field::Image < Field::File
+    dragonfly_accessor :value do
+      after_assign :resize_image
     end
 
-    validates_property :format, :of => :value, :in => ['jpeg', 'jpg', 'png', 'gif'], if: :value_uid?
-    validates_property :image?, :of => :value, :as => true, if: :value_uid?
-    validates :value, presence:true, :if => :required_field?
-
+    validates_property :format, of: :value, in: %w(jpeg jpg png gif), if: :value_uid?
+    validates_property :image?, of: :value, as: true, if: :value_uid?
+    validates :value, presence:true, if: :required_field?
 
     def default
-      # Dragonfly.app.generate(:plasma, [400, 800].sample, 600) 
-      Dragonfly.app.generate(:plain, [400, 800].sample, 600, :color => '#CCCCCC')
+      Dragonfly.app.generate :plain, [400, 800].sample, 600, color: '#CCCCCC'
     end
 
-  private
+    private
 
     def resize_image
-      self.value.process!(:resize, '1920x1080>') if value_uid? #max image size 
+      self.value.process! :resize, '1920x1080>' if value_uid? # TODO: Max image size?
     end
 
     protected
