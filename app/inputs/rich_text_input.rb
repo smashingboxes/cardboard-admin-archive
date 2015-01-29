0ed95daf10024333ae22018740c97758fdc15130
Ian Donovan <idonovan90@gmail.com>
2015-01-29 14:14:03 -0500

    Joe's underscore param point

--- app/inputs/rich_text_input.rb
+++ app/inputs/rich_text_input.rb
@@ -1,5 +1,5 @@
 class RichTextInput < SimpleForm::Inputs::TextInput
-  def input(wrapper_options = nil)
+  def input(_wrapper_options = nil)
     template.render('cardboard/fields/markdown_with_preview', f: @builder, attribute_name: attribute_name)
   end
 end
