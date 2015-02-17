94a4b0ef00f8cc8f5f610a5d3ac3e0c7ad4564e6
grettaface <bretkruse@Brets-MacBook-Pro.local>
2015-02-17 13:02:15 -0500

    Adds sidebar styling and includes the scss linter

--- app/assets/javascripts/cardboard/datepicker.js
+++ app/assets/javascripts/cardboard/datepicker.js
@@ -3,10 +3,7 @@ var create_datepickers = function(){
   $('input.datepicker').datepicker({format: 'yyyy-mm-dd'}).on('changeDate', function() {
     $(this).data('datepicker').hide();
   });
-
-  // $('input.datetimepicker').datepicker({format: "yyyy-mm-dd HH:mm pp", pick12HourFormat: true}).on('changeDate', function() {
-  //   $(this).data('datepicker').hide();
-  // });
+  
 }
 
 
